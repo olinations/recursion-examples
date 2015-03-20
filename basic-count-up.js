90fb62d209c64d2480b727098f7e09a0d34ec5dd
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-20 01:30:58 -0500

    finish basicCountUp function

--- basic-count-up.js
+++ basic-count-up.js
@@ -1,11 +1,11 @@
 // Logs and appends every number from start to end
 var basicCountUp = function(start, end) {
-  
   var current = start;
+  $('.basic-count-up-display').html('');
 
   var basicCountUpRecursion = function(current, end) {
-    if (current <= end) {
-      console.log(current);
+    if (parseInt(current) <= parseInt(end)) {
+      $('.basic-count-up-display').append('<div>' + current + '</div>');
       current++;
       basicCountUpRecursion(current, end);
     }
@@ -15,4 +15,11 @@ var basicCountUp = function(start, end) {
 
 };
 
-basicCountUp(3,11);
+// Calls function on submit
+$('.basic-count-up-submit').click(function() {
+  var start = $('.basic-count-up-start').val();
+  var end = $('.basic-count-up-end').val();
+  $('.basic-count-up-start').val('');
+  $('.basic-count-up-end').val('');
+  basicCountUp(start, end);
+});
