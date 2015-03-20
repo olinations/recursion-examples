9e8fadc9484fc5cae24f081f13e41df0f10d0716
Michael Cheng <cheng.c.mike@gmail.com>
2015-03-20 00:48:02 -0500

    add ace editor to index.html

---
+++ editors.js
@@ -0,0 +1,3 @@
+var editorBasicCountUp = ace.edit("editor-basic-count-up");
+    editorBasicCountUp.setTheme("ace/theme/twilight");
+    editorBasicCountUp.getSession().setMode("ace/mode/javascript");
